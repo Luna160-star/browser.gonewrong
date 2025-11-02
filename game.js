8598cfad328143d0992ff3f1e2bbe23b740e47a7
Luna160-star <rakhigoalsachieve@gmail.com>
2025-11-02 18:18:45 +0530

    apparatly all i was doing wrong was typing 'b instead of 'B' and constant instead of const, so I think im done with little javascipt going back to formatting.'

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
 const fileImg = document.querySelectorAll('.file1, .file2, .file3, .file4');
 
-constant dialog= document.getElementbyId('file-pop1');
-constant closebtn= document.getElementbyId('close');
-constant popupTitle = document.querySelector('.name');
-constant popupBody = document.getElementbyId('pop-body');
+const dialog= document.getElementById('file-pop1');
+const closebtn= document.getElementById('close');
+const popupTitle = document.querySelector('.name');
+const popupBody = document.getElementById('pop-body');
+
+fileImg.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        const clue1= button.getAttribute('data-clue') || ("Message here");
+
+        popupTitle.textContent = "Type the clue title here";
+        popupBody.textContent = "The clue here";
+
+        dialog.showModal();
+
+    });
+});
+
+closebtn.addEventListener('click', ()=> {
+    dialog.close();
+}
+);
