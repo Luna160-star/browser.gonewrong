eeea2af98ef0f373fe0dff0e4871b9211fccb3d7
Luna160-star <rakhigoalsachieve@gmail.com>
2025-11-02 17:18:30 +0530

    working on the js but i keep running into issues

---
+++ game.js
@@ -0,0 +1,6 @@
+const fileImg = document.querySelectorAll('.file1, .file2, .file3, .file4');
+
+constant dialog= document.getElementbyId('file-pop1');
+constant closebtn= document.getElementbyId('close');
+constant popupTitle = document.querySelector('.name');
+constant popupBody = document.getElementbyId('pop-body');
